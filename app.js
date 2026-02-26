0a94bd039658a62929013a2e4b593271a8320c72
molnarakos <86666839+molnarakos@users.noreply.github.com>
2026-02-26 07:34:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const mongoUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017';
 const dbName = 'elso-weboldalam';
